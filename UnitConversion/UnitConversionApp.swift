0b59c2bac6a9149d49d4548e5c601cfdd1c1eb35
acifre <78503928+acifre@users.noreply.github.com>
2023-01-23 16:23:36 -0600

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Anthony Cifre on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
